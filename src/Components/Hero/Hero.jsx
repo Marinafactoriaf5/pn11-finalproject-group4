3a01f160f9984e1488833e24ed54ab81204ef848
MarinaRull <mrullproduction@gmail.com>
2023-11-15 14:19:29 +0100

    hero 1.0

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,12 @@
 import './Hero.css'
+import HeroPicture from '../../assets/hero.jpeg'
 
 function Hero (){
     return(
-     <div>
+     <div className="bodyHero">
+            <img src={HeroPicture}></img>
+            <h1>Do you like adventures?</h1>
+            <h3>Discover the best events near you</h3>
         
      </div>
     )
