89bc18751ab476acaeb1565f48a6ea7dfe8113fd
isaiasd2712 <isaiasd2712@gmail.com>
2023-11-16 00:23:28 +0100

    Effect added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,8 +5,10 @@ function Hero (){
     return(
      <div className="bodyHero">
             <img className='imgHero' src={HeroPicture}></img>
+            <div className='textHero'>
             <h1 className='textHero1'>Do you like adventures?</h1>
             <h3 className='textHero2'>Discover the best events near you</h3>
+            </div>
         
      </div>
     )
