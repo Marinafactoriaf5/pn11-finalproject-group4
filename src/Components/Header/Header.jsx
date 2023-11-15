c4b7e5cdc89851bd55c3e3ac579cc4f2a61c0c85
isaiasd2712 <isaiasd2712@gmail.com>
2023-11-15 09:08:36 +0100

    header 0.2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,9 +5,11 @@ import Logo from '../../assets/logo.png'
 function Header (){
     return(
      <header>
-        <img src={Logo}></img>
-        <input className="searchImput" type="text" aria-labelledby="searchDiv" placeholder=" Type to search " />
+        <img src={Logo} className="logo" alt='VIVID'></img>
+        <div className='searchBar'>
+        <input className="searchInput" type="text" aria-labelledby="searchDiv" placeholder=" Type to search " />
         <button className="searchIcon" type="submit"></button>
+        </div>
      </header>   
 
     )
