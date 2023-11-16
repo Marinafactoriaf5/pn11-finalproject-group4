6a326880268de2060dc3ca1a5b699f361fde4dcc
Paul Corda <paulmatiascorda@gmail.com>
2023-11-16 14:28:12 +0100

    mejoras paul

--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
 import './Catalog.css';
 import data from '../../../db.json';
-
 const Catalog = () => {
-  const [events] = useState(data.events);
-
-  return (
-    <div>
-      {events.map((event) => (
-        <div key={event.nombre} className='card'>
-          <img
-            className='eventImage'
-            src={event.imagenes && event.imagenes.length > 0 ? event.imagenes[0] : ''}
-            alt={event.nombre}
-          />
-          <div className='eventText'>
-            <h4 className='eventName'>{event.nombre}</h4>
-            <p className='price'>{event.precio}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+    const [events] = useState(data.events);
+    return (
+        <section className='catalog'>
+            {events.map((event) => (
+                <div key={event.nombre} className='catalogCard'>
+                    <img
+                        className='eventImage'
+                        src={event.imagenes && event.imagenes.length > 0 ? event.imagenes[0] : ''}
+                        alt={event.nombre}
+                    />
+                    <div className='eventText'>
+                        <h4 className='eventName'>{event.nombre}</h4>
+                        <p className='price'>{event.precio}</p>
+                    </div>
+                </div>
+            ))}
+        </section>
+    );
 };
-
 export default Catalog;
+
