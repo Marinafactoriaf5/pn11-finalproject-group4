0911f7df5d6e12f5ebb7a1b383e769b3d323b34b
MarinaRull <mrullproduction@gmail.com>
2023-11-22 11:13:01 +0100

    pull marina with new event page

--- src/Pages/EventPage/EventPage.jsx
+++ src/Pages/EventPage/EventPage.jsx
@@ -5,31 +5,22 @@ import EventDescription from "../../Components/EventDescription/EventDescription
 import {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 
-function EventPage() {
+function EventPage({params}) {
   const { id } = useParams();
-  useEffect(() => {
-    const fetchItem = async () => {
-      const eventPath = "event/" + id;
-    };
-    fetchItem();
-  }, [id]);
-  const [eventData, setEventData] = useState({ events: [] });
-
+  const eventPath = "event/" + id;
+  console.log(params)
   useEffect(() => {
     fetch("../../../db.json")
       .then((response) => response.json())
       .then((data) => setEventData(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-
   console.log(eventData);
-
   return (
     <div>
-      <Header/>
-      <EventDescription event={eventData}/>
+      <Header />
+      <EventDescription event={eventData} />
     </div>
   );
 }
-
 export default EventPage;
