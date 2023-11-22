1f7accab1034bb4cdc7ee292f429f05ff6f0191a
isaiasd2712 <isaiasd2712@gmail.com>
2023-11-23 00:40:41 +0100

    searchBar justified end

---
+++ src/Components/Catalog/Filter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Filter.css'
+
+function Filter() {
+  return (
+    <div>
+        <button className='filterBtn'>
+
+        </button>
+    </div>
+  )
+}
+
+export default Filter
