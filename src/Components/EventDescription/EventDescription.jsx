568148957282796ac9e993548f56eb7de81fb185
MarinaRull <mrullproduction@gmail.com>
2023-11-23 11:57:31 +0100

    por fin funciona el p evento

--- src/Components/EventDescription/EventDescription.jsx
+++ src/Components/EventDescription/EventDescription.jsx
@@ -2,27 +2,47 @@ import EventDescriptionHero from "../EventDescription/EventDescriptionHero";
 import EventDescriptionMap from "../EventDescription/EventDescriptionMap";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./EventDescription.css";
 
 function EventDescription() {
   const { id } = useParams();
   const [eventData, setEventData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     fetch("../../../db.json")
       .then((response) => response.json())
-      .then((data) => setEventData(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-  console.log(eventData);
-  console.log(id);
+      .then((data) => {
+        const event = data.events.find((event) => event.id === parseInt(id));
+        if (event) {
+          setEventData(event);
+        } else {
+          setError(new Error("Event not found"));
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [id]);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
   return (
     <div>
       <EventDescriptionHero />
       <div className="eventData">
-          <h1>{eventData.name}</h1>
+        <h1>{eventData.name}</h1>
+        <img src={eventData.picture}/>
       </div>
       <EventDescriptionMap />
     </div>
   );
 }
-
 export default EventDescription;
