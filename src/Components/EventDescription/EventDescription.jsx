cc4cb22d5181c0845fe755891951176599e887f0
MarinaRull <mrullproduction@gmail.com>
2023-11-23 11:30:17 +0100

    intento fallido de conectar el evento con base se datos

--- src/Components/EventDescription/EventDescription.jsx
+++ src/Components/EventDescription/EventDescription.jsx
@@ -1,12 +1,25 @@
 import EventDescriptionHero from "../EventDescription/EventDescriptionHero";
 import EventDescriptionMap from "../EventDescription/EventDescriptionMap";
-// import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function EventDescription({id}) {
-console.log (id);
+function EventDescription() {
+  const { id } = useParams();
+  const [eventData, setEventData] = useState({});
+  useEffect(() => {
+    fetch("../../../db.json")
+      .then((response) => response.json())
+      .then((data) => setEventData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+  console.log(eventData);
+  console.log(id);
   return (
     <div>
       <EventDescriptionHero />
+      <div className="eventData">
+          <h1>{eventData.name}</h1>
+      </div>
       <EventDescriptionMap />
     </div>
   );
