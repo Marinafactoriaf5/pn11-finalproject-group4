ac5c7e735e84b239ce4d63fdc631ffcf05782431
isaiasd2712 <isaiasd2712@gmail.com>
2023-11-23 16:30:12 +0100

    Logo redirect

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,9 @@ import Logo from "../../assets/logo.png";
 function Header() {
   return (
     <header>
+       <a href="/" className="logo-link">
       <img src={Logo} className="logo" alt="VIVID" />
+      </a>
       <a className="navLogin" href="loginPage">LoginPage</a>
     </header>
   );
